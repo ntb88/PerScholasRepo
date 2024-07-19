dcb14894fba2ec2f5d02e18d78b4ede8437e9c81
Nikhil <nikhiltomb@gmail.com>
2024-07-19 12:52:22 -0500

    Delete src/main/java/org/example copy directory

--- src/main/java/org/example copy/WrapperClass.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class WrapperClass {
-    public static void main(String[] args) {
-        //Integer, Double, Char, Boolean
-        //every primitive have a class associated with it. They are not an object
-    }
-}
